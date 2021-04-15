78bb922c44d70d590ed17ce90a806ea4cc63dec0
Omar Aguinaga <omar.aguinaga94@gmail.com>
2021-04-15 13:02:14 +0200

    add form for creating a new Product

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,66 @@
+import useForm from '../lib/useForm'
+import Form from './styles/Form'
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
+    name: 'Stickit',
+    price: 1000,
+    description: 'Nice sticker',
+  })
+  return (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault()
+        console.log(inputs)
+      }}
+    >
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Price
+          <textarea
+            id="description"
+            name="description"
+            placeholder="description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
+  )
+}
